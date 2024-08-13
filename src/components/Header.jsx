94c60d2f22e3a8a3e6f54df657313dfa158a0e91
dnhldbrndt <dnlhldbrndt@gmail.com>
2024-08-13 03:00:56 +0200

    Update components, css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
   return (
     <div id="main">
         <div className="header-heading">
-            <h2> STEP UP YOUR</h2>
-            <h1><span>FITNESS</span> WITH US</h1>
-            <p className="details"> Build Your Body and Fintness With Professional Touch</p>
+            <h2>REACH YOUR</h2>
+            <h1><span>FITNESS</span> GOALS</h1>
+            <p className="details"> Perfect Your Body and Fitness With Expert Training and Guidance</p>
             <div className="header-btns">
                 <a href="#" className="header-btn"> Join Us</a>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,19 +15,21 @@ function Navbar() {
   return (
 
     <nav className={nav ? "nav active" : ""}>
-        <Link to='#' className="logo">
-            <img src={logo} alt=""></img>
-        </Link> 
+
+        <Link to='main' className="logo">
+      
+            <span className="nav-title">GLOBO GYM</span>
+        </Link>                     
         <input className="menu-btn" type="checkbox" id="menu-btn"/>
             <label className="menu-icon" for="menu-btn">
                 <span className="nav-icon"></span>
             </label>
             <ul className="menu">
-                <li><Link to="#">Home</Link></li>
-                <li><Link to="#">Features</Link></li>
-                <li><Link to="#">Offer</Link></li>
-                <li><Link to="#">About</Link></li>
-                <li><Link to="#">Contact</Link></li>
+                <li><Link to="main" smooth={true} duration={1000}>Home</Link></li>
+                <li><Link to="features" smooth={true} duration={1000}>Features</Link></li>
+                <li><Link to="offer" smooth={true} duration={1000}>Offer</Link></li>
+                <li><Link to="about" smooth={true} duration={1000}>About</Link></li>
+                <li><Link to="contact" smooth={true} duration={1000}>Contact</Link></li>
             </ul>
     </nav>
   )
